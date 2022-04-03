74e11ea90bda0db37a0048f33e958dd410e99c44
woinshet1 <woinshet@DESKTOP-VDL1QKS.localdomain>
2022-04-03 04:03:01 -0400

    complet assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+const findMatching = (arr, str) => {
+    return arr.filter(f => {
+        return f.toLowerCase() === str.toLowerCase();
+    })
+}
+
+const fuzzyMatch = (arr, str) => {
+    return arr.filter(f => {
+        return f.slice(0,str.length) === str;
+    })
+}
+
+const matchName = (arr, str) => {
+    return arr.filter(f => {
+        return f.name === str;
+    })
+}
